3c3ad5222252ca101e843ebfe3b84a2b336c8ede
Alex Wang <a2131665488@gmail.com>
2018-07-20 15:37:17 +0800

    modify TFIDF_PCA_Kmeans

--- week3/TFIDF_PCA_KMEANS.R
+++ week3/TFIDF_PCA_KMEANS.R
@@ -1,16 +1,11 @@
-library(bitops)
-library(httr)
-library(RCurl)
 library(tm)
-library(NLP)
 library(tmcn)
 library(jiebaRD)
 library(jiebaR)
 library(devtools)
-install_github("ggbiplot", "vqv")
-library(scales)
-library(grid)
+install_github("ggbiplot", "vqv") #下載一次就好了
 library(ggbiplot)
+library(factoextra)
 
 # INPUT DATA
 d.corpus <- Corpus( DirSource("./DATA") )
@@ -51,7 +46,7 @@ tf <- apply(as.matrix(TDM[,2:(n+1)]), 2, sum)
 library(Matrix)
 idfCal <- function(word_doc)
 { 
-  log2( n / nnzero(word_doc) ) 
+  log2( (n+1) / nnzero(word_doc) ) 
 }
 idf <- apply(as.matrix(TDM[,2:(n+1)]), 1, idfCal)
 
@@ -60,19 +55,22 @@ tempY = matrix(rep(c(as.matrix(tf)), each = length(idf)), nrow = length(idf))
 tempX = matrix(rep(c(as.matrix(idf)), each = length(tf)), ncol = length(tf), byrow = TRUE)
 doc.tfidf[,2:(n+1)] <- (doc.tfidf[,2:(n+1)] / tempY) * tempX
 
-stopLine = rowSums(doc.tfidf[,2:(n+1)])
-delID = which(stopLine == 0)
+# 如果 idf 算法是用 n+1 當分子，就不會有 0 的 entry 出現
+# stopLine = rowSums(doc.tfidf[,2:(n+1)])
+# delID = which(stopLine == 0)
 
 # PCA
 t = as.data.frame(t(doc.tfidf))
 t = t[-1,]
-t = apply(t[,1:10], 2, as.numeric)
-t = apply(t[,1:10], 1, as.numeric)
+# t = apply(t[,1:10], 2, as.numeric)
+t = apply(t, 2, as.numeric)
 pcat = prcomp(t)
 g <- ggbiplot(pcat, obs.scale = 1, var.scale = 1, ellipse = TRUE, circle = TRUE)
+g
+fviz_pca_ind(pcat)
 
 # Kmeans
-kmeansData = pcat$rotation[,1:2]
-cl <- kmeans(kmeansData, 2)
+kmeansData = pcat$x[,1:2]
+cl <- kmeans(kmeansData, 3)
 plot(kmeansData, col = cl$cluster)
 points(cl$centers, col = 1:2, pch = 8, cex = 2)
